e783b7461ddc8eb0115257d53aa4e06330fe0ce1
D98santos <96990481+D98santos@users.noreply.github.com>
2022-06-15 07:33:43 -0300

    add function range form

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document
   .addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
   );
+
+function exibValor(newValue) {
+  document.getElementById("exibPercent").innerHTML = newValue;
+}
